f2fbbc137066f3c39a276f989799c32d0526f15f
Projektsnake <projektsnake@gmail.com>
2011-03-16 19:19:03 -0700

    Implemented Troymium ontop of Abyss. Troymium will take care of our user interface by allowing us to use html and other web building languages.
    TODO:
    Make it so that the html file is computer independent -- currently points to a directory in my computer.(IN ORDER FOR CODE TO RUN: switch line 159 in AbyssGame to match your file directory)

--- Abyss/Abyss/AbyssGame.cs
+++ Abyss/Abyss/AbyssGame.cs
@@ -11,6 +11,13 @@ using Microsoft.Xna.Framework.Media;
 using FarseerPhysics.DebugViewXNA;
 using Abyss.Code.Game;
 using Abyss.Code.Screen;
+using Microsoft.Xna.Framework.Net;
+using Microsoft.Xna.Framework.Storage;
+using Troymium.Managed;
+using System.Runtime.InteropServices;
+using System.Reflection;
+using Forms = System.Windows.Forms;
+using System.Text;
 
 
 using Abyss.Code.UserInterface.OSD;
@@ -24,6 +31,28 @@ namespace Abyss
     {
         public int ScreenWidth = 1280;
         public int ScreenHeight = 720;
+
+		#region TroymiumInit
+		[DllImport("user32.dll")]
+		static extern int ToUnicode(
+			uint wVirtKey, uint wScanCode, byte[] lpKeyState,
+			[Out, MarshalAs(UnmanagedType.LPWStr, SizeConst = 64)] StringBuilder pwszBuff,
+			int cchBuff, uint wFlags
+		);
+
+		byte[] temporaryByteBuffer;
+		Texture2D backingStore;
+		Window webKit;
+		int fadeDirection = 0;
+		long? fadingSince = null;
+		Context context;
+		KeyboardState oldKB;
+		KeyboardState newKB;
+		MessageHook textInputHandler;
+
+		MouseState oldMouseState;
+		long lastKeystrokeTime = 0;
+		#endregion
 
         #region Static Members
         public static ContentManager Assests;
@@ -43,6 +72,34 @@ namespace Abyss
             Content.RootDirectory = "Content";
            // Content.Load<Texture2D>("lud_desaturated_sprite");
             Assests = Content;
+
+			// ADDED FOR SUPPORTING TROYMIUM
+			graphics.PreferredBackBufferWidth = 1024;
+			graphics.PreferredBackBufferHeight = 768;
+			graphics.PreferredBackBufferFormat = SurfaceFormat.Color;
+			graphics.PreferredDepthStencilFormat = DepthFormat.Depth16;
+			graphics.PreferMultiSampling = false;
+
+			textInputHandler = new MessageHook(Window.Handle);
+			textInputHandler.MessageReceived += (sender, e) =>
+			{
+				Forms.Message msg = textInputHandler.LastMessage;
+				switch (msg.Msg)
+				{
+					case MessageHook.WM_KEYDOWN:
+						webKit.KeyEvent(true, GetModifiers(), msg.WParam.ToInt32(), 0);
+						break;
+					case MessageHook.WM_KEYUP:
+						webKit.KeyEvent(false, GetModifiers(), msg.WParam.ToInt32(), 0);
+						break;
+					case MessageHook.WM_CHAR:
+						break;
+				}
+			};
+
+			TroymiumNET.Init("Content");
+			context = Context.Create();
+			// END OF TROYMIUM SUPPORT
             
             //osd = new OSD();
         }
@@ -61,8 +118,9 @@ namespace Abyss
             graphics.ApplyChanges();
             Window.Title = "Abyss v0.2";
 
+			IsMouseVisible = true; // Added for Troymium
             
-            base.Initialize();
+			base.Initialize();
         }
 
         /// <summary>
@@ -75,6 +133,33 @@ namespace Abyss
             spriteBatch = new SpriteBatch(GraphicsDevice);
             screenManager = new ScreenManager(this);
             screenManager.loadGameLevel();
+
+			// ADDED FOR TROYMIUM
+
+			// We have to inject our teardown code here because if we let XNA's
+			//  normal disposal handlers run, Chrome's message pump gets confused
+			//  and hangs our process
+			backingStore = new Texture2D(
+				GraphicsDevice,
+				GraphicsDevice.Viewport.Width, GraphicsDevice.Viewport.Height, false, SurfaceFormat.Color);
+
+			// Create a temporary buffer for SetData/GetData
+			temporaryByteBuffer = new byte[4 * GraphicsDevice.Viewport.Width * GraphicsDevice.Viewport.Height];
+
+			webKit = new Troymium.Managed.Window(context);
+			webKit.Paint += WebKit_Paint;
+			webKit.StartLoading += WebKit_StartLoading;
+			webKit.Load += WebKit_Load;
+			webKit.NavigationRequested += delegate(Troymium.Managed.Window win, NavigationRequestedEventArgs arg)
+			{
+				if (arg.UserGesture) win.NavigateTo(arg.Url, arg.Referrer);
+			};
+			webKit.Resize(GraphicsDevice.Viewport.Width, GraphicsDevice.Viewport.Height);
+			webKit.Transparent = true;
+			webKit.NavigateTo("file:///C:/Users/Jesus/Desktop/Abyss/Abyss/AbyssContent/AbyssUI.html");
+			//webKit.LoadHTML("C:/Users/Jesus/Desktop/Abyss/Abyss/AbyssContent/AbyssUI.html");
+			Console.WriteLine("CURRENT DIRECTORY " + System.IO.Directory.GetCurrentDirectory());
+			// END OF TROYMIUM SUPPORT
 
             // TODO: use this.Content to load your game content here
         }
@@ -100,6 +185,68 @@ namespace Abyss
             Keyboard.GetState().IsKeyDown(Keys.Escape))
                 this.Exit();
 
+			// BEGIN TROYMIUM SUPPORT
+			newKB = Keyboard.GetState();
+			var newMouseState = Mouse.GetState();
+
+			var scrollDelta = newMouseState.ScrollWheelValue - oldMouseState.ScrollWheelValue;
+
+			if ((newMouseState.X != oldMouseState.X) || (newMouseState.Y != oldMouseState.Y))
+				webKit.MouseMoved(newMouseState.X, newMouseState.Y);
+
+			if (scrollDelta != 0)
+				webKit.MouseWheel(0, scrollDelta);
+
+			if (newMouseState.LeftButton != oldMouseState.LeftButton)
+				webKit.MouseButton(MouseButton.Left, newMouseState.LeftButton == ButtonState.Pressed);
+
+			if (newMouseState.MiddleButton != oldMouseState.MiddleButton)
+				webKit.MouseButton(MouseButton.Middle, newMouseState.MiddleButton == ButtonState.Pressed);
+
+			if (newMouseState.RightButton != oldMouseState.RightButton)
+				webKit.MouseButton(MouseButton.Right, newMouseState.RightButton == ButtonState.Pressed);
+
+			var buffer = new StringBuilder();
+			byte[] pressedKeys = new byte[256];
+
+			long now = DateTime.UtcNow.Ticks;
+			bool shouldRepeat = (now - lastKeystrokeTime) > TimeSpan.FromSeconds(0.15).Ticks;
+
+			for (int i = 0; i < 255; i++)
+			{
+				var k = (Keys)i;
+				bool wasDown = oldKB.IsKeyDown(k);
+				bool isDown = newKB.IsKeyDown(k);
+				if ((wasDown != isDown) || (isDown && shouldRepeat))
+				{
+					lastKeystrokeTime = now;
+					webKit.KeyEvent(isDown, 0, i, 0);
+
+					if (isDown)
+					{
+						Array.Clear(pressedKeys, 0, 256);
+
+						foreach (var pk in newKB.GetPressedKeys())
+						{
+							int ik = (int)pk;
+
+							if ((pk == Keys.LeftShift) || (pk == Keys.RightShift))
+								ik = 0x0010;
+
+							pressedKeys[ik] = 255;
+						}
+
+						int chars = ToUnicode((uint)i, 0, pressedKeys, buffer, 1, 0);
+						if (chars > 0)
+							webKit.TextEvent(buffer.ToString(0, chars));
+					}
+				}
+			}
+			oldMouseState = newMouseState;
+			TroymiumNET.Update();
+			oldKB = newKB;
+			// END OF TROYMIUM SUPPORT
+
             screenManager.update(gameTime);
             base.Update(gameTime); //updates all GameComponents (GameObjects)
 			Input.updateInput();
@@ -123,9 +270,129 @@ namespace Abyss
             osd.Draw(gameTime);
             spriteBatch.End();*/
 
-            screenManager.drawActiveScreen(gameTime);
+			screenManager.drawActiveScreen(gameTime);
+
+			// ADDED FOR TROYMIUM
+			byte a = 255;
+			long now = DateTime.UtcNow.Ticks;
+			long elapsed = now - fadingSince.GetValueOrDefault(0);
+			if (fadeDirection > 0)
+			{
+				a = (byte)MathHelper.Clamp(255 * elapsed / TimeSpan.FromSeconds(0.25).Ticks, 0, 255);
+			}
+			else if (fadeDirection < 0)
+			{
+				a = (byte)(255 - MathHelper.Clamp(255 * elapsed / TimeSpan.FromSeconds(0.25).Ticks, 0, 255));
+			}
+
+			spriteBatch.Begin();
+			spriteBatch.Draw(backingStore, new Rectangle(0, 0, GraphicsDevice.Viewport.Width, GraphicsDevice.Viewport.Height), new Color(255, 255, 255, a));
+			spriteBatch.End();
+			// END OF TROYMIUM SUPPORT
+
 
             base.Draw(gameTime);
         }
-    }
+
+		// START OF TROYMIUM METHODS
+		protected KeyModifier GetModifiers() {
+			KeyboardState kb = Keyboard.GetState();
+			KeyModifier result = 0;
+
+			if (kb.IsKeyDown(Keys.LeftAlt) || kb.IsKeyDown(Keys.RightAlt)) result |= KeyModifier.ALT_MOD;
+			if (kb.IsKeyDown(Keys.LeftControl) || kb.IsKeyDown(Keys.RightControl)) result |= KeyModifier.CONTROL_MOD;
+			if (kb.IsKeyDown(Keys.LeftShift) || kb.IsKeyDown(Keys.RightShift)) result |= KeyModifier.SHIFT_MOD;
+
+			return result;
+		}
+
+		private void WebKit_StartLoading(Window window, UrlEventArgs args)
+		{
+			fadeDirection = -1;
+			fadingSince = DateTime.UtcNow.Ticks;
+		}
+
+		private void WebKit_Load(Window window)
+		{
+			if (fadeDirection == -1)
+			{
+				fadeDirection = 1;
+				fadingSince = DateTime.UtcNow.Ticks;
+			}
+		}
+
+		private void WebKit_Paint(Window window, PaintEventArgs args)
+		{
+			if (args.SourceBufferRect.Width <= 0 || args.SourceBufferRect.Height <= 0)
+			{
+				return;
+			}
+			int sourceBufferSize = args.SourceBufferRect.Width * args.SourceBufferRect.Height * args.SourceBufferRect.BytePerPixel;
+
+			GraphicsDevice.Textures[0] = null;
+
+			if (args.X != 0 || args.Y != 0)
+			{
+				var sourceRect = new Rectangle(args.ScrollRect.Left, args.ScrollRect.Top, args.ScrollRect.Width, args.ScrollRect.Height);
+				var destRect = sourceRect;
+				destRect.X += args.X;
+				destRect.Y += args.Y;
+				
+				var overlap = Rectangle.Intersect(destRect, sourceRect);
+
+				// We need to handle scrolling to the left
+				if (destRect.Left < 0)
+				{
+					sourceRect.X -= destRect.Left;
+					destRect.X = 0;
+				}
+				// And upward
+				if (destRect.Top < 0)
+				{
+					sourceRect.Y -= destRect.Top;
+					destRect.Y = 0;
+				}
+
+				destRect.Width = sourceRect.Width = overlap.Width;
+				destRect.Height = sourceRect.Height = overlap.Height;
+
+				if ((sourceRect.Width > 0) && (sourceRect.Height > 0))
+				{
+					// Copy the scrolled portion out of the texture into our temporary buffer
+					backingStore.GetData<byte>(0, sourceRect, temporaryByteBuffer, 0, sourceRect.Width * sourceRect.Height * 4);
+				
+					// And then copy it back into the new location
+					backingStore.SetData<byte>(0, destRect, temporaryByteBuffer, 0, destRect.Width * destRect.Height * 4);
+				}
+			}
+
+			for (int i = 0; i < args.CopyRects.Length; i++)
+			{
+				var temptRectDest = new Rectangle(args.CopyRects[i].Left, args.CopyRects[i].Top, args.CopyRects[i].Width, args.CopyRects[i].Height);
+				var temptRectSource = new Rect(args.CopyRects[i].Left - args.SourceBufferRect.Left, args.CopyRects[i].Top - args.SourceBufferRect.Top, args.CopyRects[i].Width, args.CopyRects[i].Height);
+				TroymiumNET.CopyArea(args.SourceBuffer, args.SourceBufferRect.Stride, temptRectSource, temporaryByteBuffer);				
+
+				// Ugh. Why doesn't SetData accept a pointer? Terrible.
+				var copySize = temptRectSource.Width * temptRectSource.Height * 4;
+
+
+				for (int j = 0; j < copySize; j += 4) { 
+					byte temp = temporaryByteBuffer[j]; 
+					temporaryByteBuffer[j] = temporaryByteBuffer[j + 2]; 
+					temporaryByteBuffer[j + 2] = temp; 
+				}
+				
+				backingStore.SetData<byte>(0, temptRectDest, temporaryByteBuffer, 0, copySize);
+			}
+		}
+
+		public void Teardown()
+		{
+			webKit.Dispose();
+			webKit = null;
+			context.Dispose();
+			TroymiumNET.Destroy();
+		}
+		// END OF TROYMIUM METHODS
+	}
 }
